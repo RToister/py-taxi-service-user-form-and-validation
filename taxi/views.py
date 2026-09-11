152c109cf8d1ba5acf1e2f4c9aeacaaaa2dd5785
Mac <mac@Macs-MacBook-Air.local>
2026-09-11 23:26:15 +0200

    Implement driver forms and validation

--- taxi/views.py
+++ taxi/views.py
@@ -1,24 +1,32 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
+from typing import Any
 
-from .models import Driver, Car, Manufacturer
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+
+from .forms import (
+    CarForm,
+    DriverCreationForm,
+    DriverLicenseUpdateForm,
+)
+from .models import Car, Driver, Manufacturer
 
 
 @login_required
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page of the site."""
 
-    num_drivers = Driver.objects.count()
-    num_cars = Car.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
+    num_drivers: int = Driver.objects.count()
+    num_cars: int = Car.objects.count()
+    num_manufacturers: int = Manufacturer.objects.count()
 
-    num_visits = request.session.get("num_visits", 0)
+    num_visits: int = request.session.get("num_visits", 0)
     request.session["num_visits"] = num_visits + 1
 
-    context = {
+    context: dict[str, Any] = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
@@ -30,8 +38,6 @@ def index(request):
 
 class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     model = Manufacturer
-    context_object_name = "manufacturer_list"
-    template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
 
@@ -64,13 +70,13 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
 
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
-    fields = "__all__"
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
-    fields = "__all__"
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
@@ -86,4 +92,46 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+    queryset = Driver.objects.all().prefetch_related(
+        "cars__manufacturer"
+    )
+
+
+class DriverCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Driver
+    form_class = DriverCreationForm
+
+
+class DriverLicenseUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Driver
+    form_class = DriverLicenseUpdateForm
+    template_name = "taxi/driver_license_update_form.html"
+
+    def get_success_url(self) -> str:
+        return reverse(
+            "taxi:driver-detail",
+            kwargs={"pk": self.object.pk},
+        )
+
+
+class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Driver
+    success_url = reverse_lazy("taxi:driver-list")
+
+
+@login_required
+def toggle_assign_to_car(
+        request: HttpRequest,
+        pk: int,
+) -> HttpResponseRedirect:
+    car = get_object_or_404(Car, pk=pk)
+    driver = request.user
+
+    if car.drivers.filter(pk=driver.pk).exists():
+        car.drivers.remove(driver)
+    else:
+        car.drivers.add(driver)
+
+    return HttpResponseRedirect(
+        reverse("taxi:car-detail", kwargs={"pk": pk})
+    )
